a0abb3a12d4e4b1da3e6a02c3dc6092e1f36b16a
MichaelStaehlin <44225319+MichaelStaehlin@users.noreply.github.com>
2018-11-13 09:14:12 +0100

    Add files via upload

---
+++ src/majesty/model/Card.java
@@ -0,0 +1,37 @@
+package majesty.model;
+
+public class Card {
+
+	    public enum Suit { Müllerin, Brauer, Hexe, Wachen, Soldat, Wirt, Adlige;
+	        @Override
+	        public String toString() {
+	            String suit = "";
+	            switch (this) {
+	            case Müllerin: suit = "muellerin"; break;
+	            case Brauer: suit = "brauer"; break;
+	            case Hexe: suit = "hexe"; break;
+	            case Wachen: suit = "wachen"; break;
+	            case Soldat: suit = "soldat"; break;
+	            case Wirt: suit = "wirt"; break;
+	            case Adlige: suit = "adlige"; break;
+	            }
+	            return suit;
+	        }
+	    };
+	    
+	    private final Suit suit;
+	    
+	    public Card(Suit suit) {
+	        this.suit = suit;
+	    }
+
+	    public Suit getSuit() {
+	        return suit;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return suit.toString();
+	    }
+	
+}
